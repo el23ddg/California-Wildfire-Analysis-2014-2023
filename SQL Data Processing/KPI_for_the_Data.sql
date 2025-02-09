876caf9e6e7cae4650199edf388c01aa6b8dbc2e
Debjani Dutta Gupta <158575148+el23ddg@users.noreply.github.com>
2025-02-10 03:34:02 +0530

    KPI analysis

---
+++ SQL Data Processing/KPI_for_the_Data.sql
@@ -0,0 +1,8 @@
+create database california_wildfire;
+select * from california_wildfire_data;
+select sum(Injuries) from california_wildfire_data;
+select sum(Fatalities) from california_wildfire_data;
+select sum(Area_Burned_Acres) from california_wildfire_data;
+select sum(Homes_Destroyed) from california_wildfire_data;
+select sum(Businesses_Destroyed) from california_wildfire_data;
+select sum(Vehicles_Damaged) from california_wildfire_data;
